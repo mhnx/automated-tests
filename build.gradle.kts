8c003e88e1ee742bc799a499a793278fc30d6315
Mahonri Maia <mahonri.maia@gmail.com>
2024-12-13 16:08:27 -0300

    primeiro teste

---
+++ build.gradle.kts
@@ -0,0 +1,25 @@
+plugins {
+    id("java")
+}
+
+group = "dev.mmaia"
+version = "1.0-SNAPSHOT"
+
+java {
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(17))
+    }
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
---
+++ src/test/java/dev/maia/FooBarTest.java
@@ -0,0 +1,14 @@
+package dev.maia;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
+public class FooBarTest {
+
+    @Test
+    void test() {
+        // fail("Not yet implemented");
+        System.out.println("A JUnit 5 test is running...");
+    }
+}
