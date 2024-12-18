346e4c3ade6fc4319e59941488a9e7cd9a672947
Mahonri Maia <mahonri.maia@gmail.com>
2024-12-18 10:47:40 -0300

    demonstra mais annotations

--- src/test/java/dev/maia/math/SimpleMathTest.java
+++ src/test/java/dev/maia/math/SimpleMathTest.java
@@ -1,8 +1,15 @@
 package dev.maia.math;
 
 import org.junit.jupiter.api.*;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 @DisplayName("Classe de testes")
@@ -117,6 +124,42 @@ public class SimpleMathTest {
 
         Double actual = simpleMath.squareRoot(144.0);
         Double expected = 12.0;
+
+        assertEquals(expected, actual);
+    }
+
+    @ParameterizedTest
+    @MethodSource()
+    void sumAListOfEvenNumbers(double firstNumber, double secondNumber, double expected) {
+        double actual = simpleMath.sum(firstNumber, secondNumber);
+
+        assertEquals(expected, actual);
+    }
+
+    private static Stream<Arguments> sumAListOfEvenNumbers() {
+        return Stream.of(
+                Arguments.of(2D, 4D, 6D),
+                Arguments.of(40D, 60D, 100D),
+                Arguments.of(222D, 888D, 1110D)
+        );
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"Eloim", "Jeovah", "Michael"})
+    void testValueSourceValues(String name) {
+        assertNotNull(name);
+    }
+
+    @RepeatedTest(3)
+    @DisplayName("Teste repetido")
+    void testMultiplication(RepetitionInfo info) {
+        System.out.println("Esta é a repetição " + info.getCurrentRepetition() + " de " + info.getTotalRepetitions() + ".");
+
+        double firstNumber = 8D;
+        double secondNumber = 9D;
+        double expected = 72D;
+
+        double actual = simpleMath.multiply(firstNumber, secondNumber);
 
         assertEquals(expected, actual);
     }
