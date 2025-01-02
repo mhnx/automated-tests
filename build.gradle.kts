c0d3740690e1b5e17a71863a14b12c1f4e6dfad2
Mahonri Maia <mahonri.maia@gmail.com>
2025-01-02 15:39:29 -0300

    teste usando mockito

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,6 +18,7 @@ repositories {
 dependencies {
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+    testImplementation("org.mockito:mockito-junit-jupiter:5.14.2")
     testImplementation("org.hamcrest:hamcrest:3.0")
 }
 
